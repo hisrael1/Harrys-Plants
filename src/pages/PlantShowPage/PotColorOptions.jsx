cfdf9161bb7a43736ca252326f80c59a60533c98
hisrael1 <harrisonisrael23@gmail.com>
2025-01-22 23:58:23 -0500

    atc

--- src/pages/PlantShowPage/PotColorOptions.jsx
+++ src/pages/PlantShowPage/PotColorOptions.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { POT_COLORS } from "shared/util";
+import * as plantService from "services/plants";
 
 const PotColorOptions = (props) => {
     const { plant, currentImgIdx, setCurrentImgIdx } = props;
+    const [quantity, setQuantity] = useState(1);
 
     return (
         <div className="mt-10">
@@ -19,6 +22,30 @@ const PotColorOptions = (props) => {
                         <div className={currentImgIdx == idx ? 'text-slate-700' : 'text-slate-500'}>{img.pot_color}</div>
                     </div>
                 ))}
+            </div>
+            <div className="flex mt-10">
+                <div className="flex border border-slate-400 rounded-full text-3xl">
+                    <div className="p-1 pl-4 cursor-pointer" onClick={() => {
+                        if (quantity > 1) {
+                            setQuantity(quantity - 1)
+                        }
+                    }}>-</div>
+                    <input className="w-6 text-center bg-emerald-50 text-2xl" value={quantity} type="text"/>
+                    <div className="p-1 pr-4 cursor-pointer" onClick={() => setQuantity(quantity + 1)}>+</div>
+                </div>
+                    <button className="w-full mx-4 flex justify-center items-center bg-emerald-700 text-emerald-50 flex-1 rounded-full"
+                        onClick={async () => {
+                            const response = await plantService.addPlantToCart({
+                                id: plant.id,
+                                pot_color: plant.images[currentImgIdx].pot_color,
+                                quantity,
+                            })
+                            const data = await response.json();
+                        }}
+                    >
+                        <i className="fa-solid fa-cart-shopping text-2xl mr-2"></i>
+                        add to cart
+                    </button>
             </div>
         </div>
     )
--- src/services/plants.js
+++ src/services/plants.js
@@ -2,4 +2,10 @@ import apiFetch from "./apiFetch";
 
 export const getPlants = () => apiFetch("GET", "/plants");
 
-export const getPlantById = ({id}) => apiFetch('GET', `/plants/${id}`)
+export const getPlantById = ({ id }) => apiFetch("GET", `/plants/${id}`);
+
+export const addPlantToCart = ({ id, quantity, potColor }) =>
+  apiFetch("POST", `/cart/plants/${id}`, {
+    pot_color: potColor,
+    quantity
+  });
