f831b9f30abd82b411b02f851f8d1a6906105bac
hisrael1 <harrisonisrael23@gmail.com>
2025-01-30 18:44:38 -0500

    hosting complete + fixed checkout button

--- src/services/apiFetch.js
+++ src/services/apiFetch.js
@@ -1,12 +1,7 @@
 import * as userService from './user';
 const { VITE_API_BASE_URL, VITE_API_KEY } = import.meta.env;
 
-const apiFetch = (method, path, body = null) => {
-  console.log('API URL:', VITE_API_BASE_URL + path);
-  console.log('Environment:', import.meta.env);
-  console.log('deploy');
-
-  
+const apiFetch = (method, path, body = null) => {  
   const options = {
     method,
     credentials: "include",
